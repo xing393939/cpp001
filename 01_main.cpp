052b182fba306bdd352ee543766a8d6ceabbb228
chenxing <chenxing@qq.com>
2020-06-20 08:39:54 +0800

    1

--- 01_main.cpp
+++ 01_main.cpp
@@ -3,25 +3,11 @@
 #include <pthread.h>
 #include <unistd.h>
 
-__thread int num = 0;
+int main(void)
+{
+    int count[] = { 3, 2, }, i;
 
-void* worker(void* arg) {
-    while (1) {
-        printf("thread:%d %p\n", num, &num);
-        sleep(1);
-    }
-}
-
-int main(void) {
-    pthread_t tid;
-    int ret;
-    if (ret = pthread_create(&tid, NULL, worker, NULL) != 0) {
-        return 1;
-    }
-    while (1) {
-        num = 4;
-        printf("main:%d %p\n", num, &num);
-        sleep(1);
-    }
+    for (i = 0; i < 4; i++)
+        printf("count[%d]=%d\n", i, count[i]);
     return 0;
 }
